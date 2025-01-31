c512757fcddc84ef00437c5f88d81da862338dcf
Lucas Benkendorf <benklucass@gmail.com>
2025-01-31 10:55:24 -0300

    criado entidade de leitor

--- src/entities/Leitor.ts
+++ src/entities/Leitor.ts
@@ -1,5 +1,31 @@
-class Leitor {
-    
-}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-export default Leitor;
+@Entity("leitores")
+export class Leitor {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: "varchar", nullable: false })
+    nome: string;
+
+    @Column({ type: "varchar", unique: true })
+    email: string;
+
+    @Column({ type: "varchar" })
+    telefone: string;
+
+    @Column({ type: "date" })
+    data_nascimento: Date;
+
+    @Column({ type: "text" })
+    endereco: string;
+
+    @Column({ type: "boolean", default: true })
+    ativo: boolean;
+
+    @CreateDateColumn({ type: "timestamp" })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: "timestamp" })
+    updated_at: Date;
+}
